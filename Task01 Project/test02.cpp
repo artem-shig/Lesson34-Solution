2dad48758a2760b2679abc3a65299d709a3826d8
artem-shig <artem.shigov@gmail.com>
2025-02-16 18:20:55 +0300

    Тестирование передачи данных в функцию по значению (task02)

--- Task01 Project/test02.cpp
+++ Task01 Project/test02.cpp
@@ -1,3 +1,22 @@
 #include <iostream>
 using namespace std;
 
+void change(int num) {
+
+	num *= 10;
+
+
+}
+
+int main() {
+
+	int x = 100;
+
+	cout << "Before x =  " << x << endl;
+	
+	change(x);
+
+	cout << "After x = " << x << endl;
+
+	return 0;
+}
